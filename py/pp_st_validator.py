42d8080eadfa9365976b6fb99534696faafd7672
kevin <kjgalli@radium.ncsc.mil>
2022-09-26 13:28:47 -0400

    Seems to be somewhat working with simle depends

--- py/pp_st_validator.py
+++ py/pp_st_validator.py
@@ -9,7 +9,7 @@ import sys
 from lxml.etree import Schematron
 from  lxml.etree import Element, SubElement
 import lxml.etree
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
 
 # from io import StringIO
 # import re
@@ -24,65 +24,84 @@ def SCH(tag):
 
 class State:
 
-    def __init__(self, root):
+    def __init__(self, root, rule):
         self.root = root
         self.parent_map = {c: p for p in self.root.iter() for c in p}
-
+        self.rule = rule
         self.derive_schematron()
+
+    def is_foreign_depends(el):
+        child = el.find("./*")        
+        return child is not None
         
     def derive_schematron(self):
         for depend in self.root.findall(".//cc:depends", ns):
-            if depend.find("./*"):
+            if State.is_foreign_depends(depend):
                 print("There's a child")
             else:
                 parent = self.parent_map[depend]
                 for dependency_id in depend.attrib:
                     self.handle_dependency(parent, depend.attrib[dependency_id])
 
-
-
-
     def handle_dependency(self, dependent, dependee_id):
-
-        rul = ""
+        test = ""
         if dependent.tag == "{"+ns['cc']+"}include-pkg":
             baseurl = dependent.find(".//cc:url",ns).text;
             branch =  dependent.find(".//cc:branch",ns).text;
-            rul = ".//cc:include-pkg[.//cc:url='"+baseurl + "' and .//cc:branch='"+branch+"']"
+            test = ".//cc:include-pkg[.//cc:url='"+baseurl + "' and .//cc:branch='"+branch+"']"
         elif dependent.tag == "{"+ns['cc']+"}f-component":
             cc_id = dependent.attrib["cc-id"]
-            rul = ".//cc:f-component[@cc-id='"+cc_id
+            test = ".//cc:f-component[@cc-id='"+cc_id
             if "iteration" in dependent.attrib:
-                rul += "' and @iteration='"+dependent.attrib['iteration']
-            rul += "']"
+                test += "' and @iteration='"+dependent.attrib['iteration']
+            test += "']"
         else:
             return
-        rul += " or .//*[@id='"+dependee_id+"']"
-        print("<assert test=\""+ rul +"\"> </assert>") 
-            
-                    
-if __name__ == "__main__":
+        test += " or .//*[@id='"+dependee_id+"']"
+        add_assert(self.rule, test, "")
+
+
+def make_schematron_skeleton():
     el = Element(SCH("schema"))
     for aa in ns:
         ns_element = SubElement(el, SCH("ns"))
         ns_element.attrib["prefix"] = aa
-        ns_element.attrib["url"]= ns[aa]
+        ns_element.attrib["uri"]= ns[aa]
         print("ns["+aa+"]: " + ns[aa])
 
     patt = SubElement(el, SCH("pattern"))
     patt.attrib["name"]="Basic"
     rule = SubElement(patt, SCH("rule"))
     rule.attrib["context"]="/*"
-#    print(lxml.etree.tostring(el, pretty_print=True))
-    print(lxml.etree.tostring(el))
-    
+    return el, rule
 
-    
-    Schematron(el)
+def add_assert(rule_el, test, descrip):
+    assert_el = SubElement(rule_el, SCH("assert"))
+    assert_el.attrib["test"]=test
+    assert_el.text = descrip
+
+                    
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: <pp-xml> <st-xml>")
         sys.exit(0)
-    pp = ET.parse(sys.argv[1])
-    State(pp)
+
+    root, rule = make_schematron_skeleton()
+    add_assert(rule, "/*", "You shall not not pass.")
+    #    print(lxml.etree.tostring(el, pretty_print=True))
+    pp = lxml.etree.parse(sys.argv[1])
+    State(pp, rule)
+    print(lxml.etree.tostring(root, pretty_print=True, encoding='utf-8').decode("utf-8"))
+    schematron = Schematron(root)
+
+    st = lxml.etree.parse(sys.argv[2])
+    res = schematron.validate(st)
+    if res:
+        print("SUCCESS: "+sys.argv[2])
+    else:
+        print("FAILURE: "+sys.argv[2])
+        print(schematron.error_log)
+
+    
 
     
